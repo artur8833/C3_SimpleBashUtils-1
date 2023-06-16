575cd43b03040748e271c747e66e0bb6fd3dc629
Artur Gazizov <a.gazizov883@yandex.ru>
2023-06-16 17:53:03 +0500

    Add

---
+++ src/grep/s21_grep.h
@@ -0,0 +1,31 @@
+#ifndef SRC_GREP_S21_GREP_H_
+#define SRC_GREP_S21_GREP_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct options {
+  int e;
+  int i;
+  int v;
+  int c;
+  int l;
+  int n;
+  int s;
+  int h;
+  int o;
+  int f;
+  char template[1024];
+  int num;
+  int error;
+} opt;
+int mem_template(opt *options, char *pattern);
+void parser(int argc, char **argv, opt *options);
+void reader(char **argv, opt *options);
+int suchPattern(char *str, opt *options);
+void generalGerister(int reti);
+void from_flag_f(opt *options, char *name);
+void print_o(char *string, opt *options, char *filename, int line_number);
+
+#endif
